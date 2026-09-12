e18a5d01fa663b5849fb7b10160407545fb02c96
Muhammad Mahdy <midohoda120@gmail.com>
2026-09-12 05:21:14 +0300

    Initial BACK TO X website

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE = "back-to-x-v1";
+
+self.addEventListener("install", event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(["/", "/index.html"]))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request))
+  );
+});
